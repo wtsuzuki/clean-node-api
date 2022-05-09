469d90d6e7eb4af1e9d28d55292b17aec4365089
Willian Suzuki <willian.suzuki@valecard.com.br>
2022-05-09 19:42:23 -0300

    test: fix test comment

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -54,7 +54,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 
-  test('Should return 400 if no name is password', () => {
+  test('Should return 400 if no password is provided', () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: {
